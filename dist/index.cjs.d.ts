87623229e2a9cca4bd47ca160e5ed1a1893127e0
Fevol <vernieuwefelix@gmail.com>
2023-03-06 00:31:48 +0100

    chore: remove dist folder

--- dist/index.cjs.d.ts
+++
@@ -1,4 +0,0 @@
-import { LRLanguage, LanguageSupport } from '@codemirror/language';
-declare const criticmarkupLanguage: LRLanguage;
-declare function criticmarkup(): LanguageSupport;
-export { criticmarkupLanguage, criticmarkup };
