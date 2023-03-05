5a464bd1b9534962a0e1db6160455869204627e0
Fevol <vernieuwefelix@gmail.com>
2023-03-05 23:15:58 +0100

    chore: add parser dist to git repo

---
+++ dist/index.cjs.d.ts
@@ -0,0 +1,4 @@
+import { LRLanguage, LanguageSupport } from '@codemirror/language';
+declare const criticmarkupLanguage: LRLanguage;
+declare function criticmarkup(): LanguageSupport;
+export { criticmarkupLanguage, criticmarkup };
